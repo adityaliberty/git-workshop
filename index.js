4acab769ad7b4dbb05b2ddb889fc6b412bb04187
adityaliberty <adityaliberty33@gmail.com>
2021-04-22 17:38:25 +0530

    new change in new-feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 firstContainer.addEventListener('click',function(){
-  window.alert("clicked");
+  window.alert("clicked on this container");
 })
 firstContainer.addEventListener('mouseover',function(){
   console.log("mouse on container");
